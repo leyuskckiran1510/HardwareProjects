f270077944f9a945250edc16465f990664cd3f2e
leyuskckiran1510 <kdhakal1510@gmail.com>
2025-05-16 00:06:49 +0545

    feat: complete basic quiz wred hardware schematics and code

--- quiz/main.c
+++ quiz/main.c
@@ -34,6 +34,14 @@ _Serial Serial;
 
 #endif
 
+#define DEBUG
+
+#ifdef DEBUG 
+#define log Serial.print
+#else 
+#define log(...) 
+#endif
+
 int ranks_map[33]={0};
 int rank_record[6]={};
 
@@ -70,6 +78,26 @@ void setup(){
     reset_rank_status();
 }
 
+void print_ranks(){
+    log("Ranks\n========\n");
+    for(int i=0;i<6;i++){
+        log(i);
+        log("\t: ");
+        switch (rank_record[i]){
+            case 0b000001: {log("port8"); break;}
+            case 0b000010: {log("port9"); break;}
+            case 0b000100: {log("port10"); break;}
+            case 0b001000: {log("port11"); break;}
+            case 0b010000: {log("port12"); break;}
+            case 0b100000: {log("port13"); break;}
+            default:  log("unknown");  
+        }
+        if(i==rank_show_index) log("\t(active)");
+        log("\n");
+    }
+    log("========\n");
+}
+
 #define ITERATE_MODE 0b00
 #define RESET_MODE   0b10
 
@@ -78,24 +106,23 @@ void loop(){
     
     if(intrupt && ranks_map[intrupt] < 0 && rank_index<6){
         ranks_map[intrupt] = rank_index;
-        rank_record[rank_index] = intrupt;
+        rank_record[rank_index++] = intrupt;
     }
-    int light_to_toggle = rank_show_index<6?rank_record[rank_show_index]:0b00111111; 
+    int light_to_toggle = rank_show_index < (rank_index) ?rank_record[rank_show_index]:0b00111111; 
     int pin1Toggle = PIND&0b01;
     int pin1Mode   = PIND&0b10;
-    Serial.print(PIND,BIN);
-    Serial.print("\n");
-    PORTD ^= (light_to_toggle<<2);
+    PORTD = (light_to_toggle<<2);
 
 
     switch (pin1Mode) {
         case ITERATE_MODE:{
             if(pin1Toggle){
-                rank_show_index= (rank_show_index+1)%7;
+                rank_show_index= (rank_show_index+1)%(rank_index+1);
             }
             break;
         }
         case RESET_MODE:{
+            Serial.write("Reset Mode on \n");
             if(pin1Toggle){
                 PORTD=0;
                 reset_rank_status();
@@ -104,5 +131,6 @@ void loop(){
             break;
         }
     }
+    print_ranks();
     delay(10);
 }
